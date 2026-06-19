498611f3e0015981f7e964dd7a432bc6b0a60fd4
gaylor-bussy <gaylor.bussy@gmail.com>
2026-06-19 11:18:07 +0200

    v1.5

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,6 @@ var router = express.Router();
 var mysql = require('mysql2');
 var moment = require('moment');
 
-
-
 const db = mysql.createPool({
     host: 'localhost',     // Adresse du serveur MySQL
     user: 'root',          // Nom d'utilisateur MySQL
@@ -25,58 +23,8 @@ db.getConnection((err, connection) => {
     }
 });
 
-// Route GET : récupérer tous les utilisateurs
 
-
-// // Route POST : ajouter un utilisateur (avec requête préparée pour éviter l’injection SQL)
-// app.post('/users', (req, res) => {
-//     const { name, email } = req.body;
-
-//     // Validation basique
-//     if (!name || !email) {
-//         return res.status(400).json({ error: 'Nom et email requis' });
-//     }
-
-//     const sql = 'INSERT INTO users (name, email) VALUES (?, ?)';
-//     db.query(sql, [name, email], (err, result) => {
-//         if (err) {
-//             console.error('Erreur lors de l’insertion :', err.message);
-//             return res.status(500).json({ error: 'Erreur serveur' });
-//         }
-//         res.status(201).json({ message: 'Utilisateur ajouté', id: result.insertId });
-//     });
-// });
-
-// Lancement du serveur
-/*const PORT = 3307;
-app.listen(PORT, () => {
-    console.log(`🚀 Serveur démarré sur http://localhost:${PORT}`);
-});
-*/
-
-// app.get('/users', (req, res) => {
-//     const sql = 'SELECT* FROM user_';
-//     db.query(sql, (err, results) => {
-//         if (err) {
-//             console.error('Erreur lors de la requête :', err.message);
-//             return res.status(500).json({ error: 'Erreur serveur' });
-//         }
-//         res.json(results);
-//     });
-// });
-
-
-
-
-
-
-
-// /* POST request page  . */// // requete sql a faire : create sql table demande id auto nb demande auto description (valeur input) , date de creation auto nb afpa (valeur input nom , prénom ,matricule)
-// router.post('/invite/request', function(req, res, next) {
-
-//   res.render('index', { title: 'Express' });
-// });
-
+// /* POST Request page. */
 router.post('/invite/request', (req, res) => {
 
     const sql = `
@@ -93,7 +41,7 @@ router.post('/invite/request', (req, res) => {
             id_technicien,
             id_validateur
         )
-        VALUES (NULL, '`+ req.body.Description + `', '` + moment().format("YYYY-MM-DD") + `', '`+ req.body.Num_AFPA_invite + `', '`+ req.body.Nom_AFPA_invite + `', '`+ req.body.Prenom_AFPA_invite + `', '0', NULL, NULL, NULL, NULL)
+        VALUES (NULL, '`+ req.body.Description + `', '` + moment().format("YYYY-MM-DD") + `', '` + req.body.Num_AFPA_invite + `', '` + req.body.Nom_AFPA_invite + `', '` + req.body.Prenom_AFPA_invite + `', '0', NULL, NULL, NULL, NULL)
     `;
 
     db.query(
@@ -119,10 +67,6 @@ router.post('/invite/request', (req, res) => {
 });
 
 
-
-
-
-
 // /* GET NbRequest page. */
 router.get('/invite/request/:NbRequest', (req, res) => {
     const NbRequest = req.params.NbRequest;
@@ -141,25 +85,7 @@ router.get('/invite/request/:NbRequest', (req, res) => {
 
 
 
-
-
-
-
-
-
-// /* GET register page. */
-// router.post('/register', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-
-
-
-
-
-
-
-
+// /* GET Menu page. */
 
 router.get('/dashboard', (req, res) => {
     const sql = "SELECT *  FROM `demande` "
@@ -172,20 +98,10 @@ router.get('/dashboard', (req, res) => {
         res.json(results);
     });
 });
-
-
-
-
-
-
-
 // /* GET priority page. */
 // router.update('/dashboard/priority/:id_demande', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
-
-
-
 // /* GET réalisé page. */
 // router.update('/dashboard/realise/:id_demande', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
@@ -196,6 +112,30 @@ router.get('/dashboard', (req, res) => {
 // router.update('/dashboard/requesttaken/:id_demande', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
+router.put('/dashboard/update/:id_demande', function (req, res, next) {
+const id = req.params.id_demande;
+const sql =" UPDATE demande SET id_status = " + req.body.id_status + " WHERE id_demande=? ";
+
+ 
+  db.query(sql,[id], (err, result) => {
+       if (err) {
+                console.error("Erreur SQL :", err);
+
+                return res.status(500).json({
+                    message: err.message,
+                    code: err.code,
+                    sqlMessage: err.sqlMessage
+                });
+            } else {
+                return res.status(200).json({
+                    message: 'status modifié',
+                    code: 'OK'
+
+                });
+            }
+   
+});
+});
 
 
 
@@ -204,6 +144,167 @@ router.get('/dashboard', (req, res) => {
 
 
 
+// /* POST Inscription page. */
+
+
+const bcrypt = require('bcrypt');
+
+router.post('/register', async (req, res) => {
+    const { id_user,Nom,Prenom,Num_AFPA,Password ,id_role } = req.body;
+
+    // try {
+        const hash = await bcrypt.hash(Password, 10);
+
+        const sql = `
+            INSERT INTO user_(
+            id_user,
+            Nom,
+            Prenom, 
+            Num_AFPA,
+            Password,
+            id_role)
+            VALUES (NULL, '`+ req.body.Nom + `','`+ req.body.Prenom + `','`+ req.body.Num_AFPA + `','`+ req.body.Password + `','4')
+        `;
+
+       db.query(
+        sql,
+        (err, result) => {
+            if (err) {
+                console.error("Erreur SQL :", err);
+
+                return res.status(500).json({
+                    message: err.message,
+                    code: err.code,
+                    sqlMessage: err.sqlMessage
+                });
+            } else {
+                return res.status(200).json({
+                    message: 'compte ajoutée',
+                    code: 'OK'
+
+                });
+            }
+        })
+});
+
+
+// /* POST login page. */
+
+// const jwt = require('jsonwebtoken');
+
+// app.post('/login', (req, res) => {
+
+//     const { email, password } = req.body;
+
+//     const sql = `
+//         SELECT *
+//         FROM utilisateur
+//         WHERE email = ?
+//     `;
+
+//     db.query(sql, [email], async (err, result) => {
+
+//         if (err) {
+//             return res.status(500).json(err);
+//         }
+
+//         if (result.length === 0) {
+//             return res.status(401).json({
+//                 message: 'Utilisateur introuvable'
+//             });
+//         }
+
+//         const user = result[0];
+
+//         const isValid = await bcrypt.compare(
+//             password,
+//             user.mot_de_passe
+//         );
+
+//         if (!isValid) {
+//             return res.status(401).json({
+//                 message: 'Mot de passe incorrect'
+//             });
+//         }
+
+//         const token = jwt.sign(
+//             {
+//                 id: user.id,
+//                 email: user.email
+//             },
+//             process.env.JWT_SECRET,
+//             {
+//                 expiresIn: '24h'
+//             }
+//         );
+
+//         res.json({
+//             token
+//         });
+//     });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+// const auth = require('./middleware/auth');
+
+// app.get('/profil', auth, (req, res) => {
+
+//     res.json({
+//         message: 'Accès autorisé',
+//         user: req.user
+//     });
+
+// });
 
 
 module.exports = router;
+
+
+// const jwt = require('jsonwebtoken');
+
+// function auth(req, res, next) {
+
+//     const authHeader = req.headers.authorization;
+
+//     if (!authHeader) {
+//         return res.status(401).json({
+//             message: 'Token manquant'
+//         });
+//     }
+
+//     const token = authHeader.split(' ')[1];
+
+//     try {
+
+//         const decoded = jwt.verify(
+//             token,
+//             process.env.JWT_SECRET
+//         );
+
+//         req.user = decoded;
+
+//         next();
+
+//     } catch (error) {
+
+//         return res.status(401).json({
+//             message: 'Token invalide'
+//         });
+//     }
+// }
+
+// module.exports = auth;
+
+
+
+
